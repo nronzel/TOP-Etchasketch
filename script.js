2a8d69626ad0978818d58d54abca4658970d64b0
nronzel <86695181+nronzel@users.noreply.github.com>
2022-11-14 15:47:53 -0500

    added hover feature and clear button to clear screen

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
-const container = document.querySelector('.drawArea');
+const container = document.querySelector('.container');
+const clear = document.querySelector('.reset');
 
-console.log(container);
+// function reset(e) {
+//     e.classList.remove = 'hovered';
+// }
+
+
+function makeRows(rows, columns) {
+    container.style.setProperty('--rows', rows);
+    container.style.setProperty('--cols', columns);
+    for (let i = 0; i < (rows * columns); i++) {
+        let cell = document.createElement('div');
+        // cell.textContent = (i + 1);
+        container.appendChild(cell).className = 'gridItem';
+        cell.onmouseover = function () {
+            cell.classList = 'hovered';
+        };
+        clear.addEventListener('click', () => {
+            cell.classList.remove('hovered');
+        });
+    };
+};
+
+// clear.addEventListener('click', reset);
+
+makeRows(16,16);
